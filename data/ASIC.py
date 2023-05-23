085aba6930999e2bd913f7de933e90467ec3223e
ihassan1 <ihassan1@kpmg.com.au>
2023-05-24 02:47:18 +1000

    added ASIC

---
+++ data/ASIC.py
@@ -0,0 +1,354 @@
+data = [
+  {
+    "timestamp": "2022-01-20T07:45:22Z",
+    "text": "ASIC releases new guidelines for digital currency exchanges."
+  },
+  {
+    "timestamp": "2022-01-20T13:20:15Z",
+    "text": "Important update from ASIC regarding financial reporting requirements."
+  },
+  {
+    "timestamp": "2022-01-21T08:55:47Z",
+    "text": "ASIC launches an investigation into insider trading allegations."
+  },
+  {
+    "timestamp": "2022-01-21T16:10:36Z",
+    "text": "Reminder: ASIC's annual conference on regulatory compliance is just around the corner."
+  },
+  {
+    "timestamp": "2022-01-22T10:30:12Z",
+    "text": "ASIC issues a warning about potential investment scams in the cryptocurrency market."
+  },
+  {
+    "timestamp": "2022-01-22T18:05:55Z",
+    "text": "ASIC publishes a report on the state of financial markets in Australia."
+  },
+  {
+    "timestamp": "2022-01-23T09:15:30Z",
+    "text": "ASIC announces new licensing requirements for financial advisers."
+  },
+  {
+    "timestamp": "2022-01-23T14:40:14Z",
+    "text": "ASIC reminds companies to submit their annual reports by the deadline."
+  },
+  {
+    "timestamp": "2022-01-24T08:20:22Z",
+    "text": "ASIC takes legal action against a company for breaching financial regulations."
+  },
+  {
+    "timestamp": "2022-01-24T15:55:10Z",
+    "text": "ASIC's website will be undergoing maintenance tonight. Apologies for any inconvenience caused."
+  },
+  {
+    "timestamp": "2022-01-25T10:10:35Z",
+    "text": "ASIC launches a new online portal for submitting regulatory filings."
+  },
+  {
+    "timestamp": "2022-01-25T17:35:29Z",
+    "text": "ASIC hosts a workshop on cybersecurity practices for financial institutions."
+  },
+  {
+    "timestamp": "2022-01-26T11:25:14Z",
+    "text": "Happy Australia Day! ASIC's offices will be closed today, and normal operations will resume tomorrow."
+  },
+  {
+    "timestamp": "2022-01-26T14:55:47Z",
+    "text": "ASIC issues a warning about a fraudulent investment scheme targeting elderly citizens."
+  },
+  {
+    "timestamp": "2022-01-27T09:40:33Z",
+    "text": "ASIC partners with industry experts to develop new regulations for digital asset exchanges."
+  },
+  {
+    "timestamp": "2022-01-27T16:15:25Z",
+    "text": "ASIC publishes a guide to help businesses comply with new anti-money laundering laws."
+  },
+  {
+    "timestamp": "2022-01-28T08:50:19Z",
+    "text": "ASIC's annual report for the previous financial year is now available on our website."
+  },
+  {
+    "timestamp": "2022-01-28T14:15:07Z",
+    "text": "ASIC investigates a case of potential market manipulation in the stock market."
+  },
+  {
+    "timestamp": "2022-01-29T09:30:52Z",
+    "text": "ASIC launches a public consultation on proposed changes to crowdfunding regulations."
+  },
+  {
+    "timestamp": "2022-01-29T17:05:41Z",
+    "text": "Reminder: ASIC's licensing fees are due for payment by the end of this month."
+  },
+  {
+    "timestamp": "2022-01-30T10:15:29Z",
+    "text": "ASIC appoints a new Commissioner to oversee consumer protection initiatives."
+  },
+  {
+    "timestamp": "2022-01-30T16:40:12Z",
+    "text": "ASIC releases a report on the impact of climate change on financial markets."
+  },
+  {
+    "timestamp": "2022-01-31T08:10:18Z",
+    "text": "ASIC imposes penalties on a financial institution for breaching responsible lending obligations."
+  },
+  {
+    "timestamp": "2022-01-31T14:35:06Z",
+    "text": "ASIC launches an education campaign to raise awareness about investment scams."
+  },
+  {
+    "timestamp": "2022-02-01T09:20:30Z",
+    "text": "ASIC provides guidance on the use of artificial intelligence in financial services."
+  },
+  {
+    "timestamp": "2022-02-01T15:45:21Z",
+    "text": "ASIC's online registry will be temporarily unavailable for maintenance tomorrow."
+  },
+  {
+    "timestamp": "2022-02-02T10:05:45Z",
+    "text": "ASIC grants relief to small businesses affected by COVID-19-related financial challenges."
+  },
+  {
+    "timestamp": "2022-02-02T17:30:38Z",
+    "text": "ASIC investigates allegations of misconduct by a prominent financial advisor."
+  },
+  {
+    "timestamp": "2022-02-03T11:15:12Z",
+    "text": "ASIC launches a new initiative to promote financial literacy among young Australians."
+  },
+  {
+    "timestamp": "2022-02-03T14:45:54Z",
+    "text": "ASIC warns consumers about the risks of investing in speculative cryptocurrencies."
+  },
+  {
+    "timestamp": "2022-02-04T09:40:27Z",
+    "text": "ASIC releases a consultation paper on proposed changes to whistleblower protection laws."
+  },
+  {
+    "timestamp": "2022-02-04T17:05:13Z",
+    "text": "ASIC's annual enforcement report highlights significant regulatory actions taken in the past year."
+  },
+  {
+    "timestamp": "2022-02-05T10:20:35Z",
+    "text": "ASIC launches an investigation into a company's failure to disclose material information."
+  },
+  {
+    "timestamp": "2022-02-05T15:55:24Z",
+    "text": "ASIC reminds financial advisers to renew their professional indemnity insurance."
+  },
+  {
+    "timestamp": "2022-02-06T08:30:19Z",
+    "text": "ASIC partners with international regulators to combat cross-border financial fraud."
+  },
+  {
+    "timestamp": "2022-02-06T14:55:08Z",
+    "text": "ASIC proposes changes to the licensing requirements for credit providers."
+  },
+  {
+    "timestamp": "2022-02-07T09:40:42Z",
+    "text": "ASIC launches an inquiry into the conduct of insurance companies in relation to claims handling."
+  },
+  {
+    "timestamp": "2022-02-07T17:05:37Z",
+    "text": "ASIC publishes a guide to help small businesses understand their superannuation obligations."
+  },
+  {
+    "timestamp": "2022-02-08T10:15:54Z",
+    "text": "ASIC warns consumers about the risks of unlicensed binary options trading platforms."
+  },
+  {
+    "timestamp": "2022-02-08T16:40:47Z",
+    "text": "ASIC's online services will be temporarily unavailable this weekend due to scheduled maintenance."
+  },
+  {
+    "timestamp": "2022-02-09T11:05:22Z",
+    "text": "ASIC releases a consultation paper on proposed changes to responsible lending laws."
+  },
+  {
+    "timestamp": "2022-02-09T14:35:05Z",
+    "text": "ASIC investigates a case of suspected market manipulation in the foreign exchange market."
+  },
+  {
+    "timestamp": "2022-02-10T08:20:39Z",
+    "text": "ASIC launches a new campaign to encourage Australians to check their superannuation accounts."
+  },
+  {
+    "timestamp": "2022-02-10T14:45:28Z",
+    "text": "ASIC imposes additional conditions on a financial services licensee for non-compliance."
+  },
+  {
+    "timestamp": "2022-02-11T09:30:13Z",
+    "text": "ASIC releases a report on the performance of Australia's financial markets in the past quarter."
+  },
+  {
+    "timestamp": "2022-02-11T17:05:01Z",
+    "text": "ASIC warns consumers about the risks associated with high-frequency trading strategies."
+  },
+  {
+    "timestamp": "2022-02-12T10:15:45Z",
+    "text": "ASIC publishes a guide on responsible lending obligations for credit providers."
+  },
+  {
+    "timestamp": "2022-02-12T16:40:36Z",
+    "text": "ASIC investigates a case of potential misconduct by a financial planning firm."
+  },
+  {
+    "timestamp": "2022-02-13T11:25:21Z",
+    "text": "ASIC launches an online tool to help consumers compare and select superannuation products."
+  },
+  {
+    "timestamp": "2022-02-13T14:55:53Z",
+    "text": "ASIC reminds companies to submit their annual statements by the deadline."
+  },
+  {
+    "timestamp": "2022-02-14T09:40:38Z",
+    "text": "ASIC imposes penalties on a financial institution for breaching best interest duty obligations."
+  },
+  {
+    "timestamp": "2022-02-14T17:05:26Z",
+    "text": "ASIC releases a guide to help consumers understand their rights when dealing with debt collectors."
+  },
+  {
+    "timestamp": "2022-02-15T10:10:49Z",
+    "text": "ASIC hosts a webinar on cybersecurity practices for small businesses."
+  },
+  {
+    "timestamp": "2022-02-15T17:35:33Z",
+    "text": "ASIC publishes a report on the outcomes of its recent enforcement actions."
+  },
+  {
+    "timestamp": "2022-02-16T11:25:18Z",
+    "text": "ASIC launches a new online portal for company registration and lodgement of documents."
+  },
+  {
+    "timestamp": "2022-02-16T14:55:51Z",
+    "text": "ASIC investigates a case of potential insider trading by company executives."
+  },
+  {
+    "timestamp": "2022-02-17T09:40:37Z",
+    "text": "ASIC announces changes to the regulatory framework for credit rating agencies."
+  },
+  {
+    "timestamp": "2022-02-17T17:05:21Z",
+    "text": "ASIC's annual report on the supervision of markets and infrastructure is now available."
+  },
+  {
+    "timestamp": "2022-02-18T10:20:43Z",
+    "text": "ASIC issues a warning about a fraudulent investment scheme targeting vulnerable individuals."
+  },
+  {
+    "timestamp": "2022-02-18T15:55:32Z",
+    "text": "ASIC reminds financial advisers to update their details on the Financial Advisers Register."
+  },
+  {
+    "timestamp": "2022-02-19T09:40:29Z",
+    "text": "ASIC launches an investigation into potential breaches of continuous disclosure obligations."
+  },
+  {
+    "timestamp": "2022-02-19T17:05:17Z",
+    "text": "ASIC publishes a guide to help consumers understand the risks of margin lending."
+  },
+  {
+    "timestamp": "2022-02-20T10:20:40Z",
+    "text": "ASIC releases a consultation paper on proposed changes to crowdfunding regulations."
+  },
+  {
+    "timestamp": "2022-02-20T15:55:29Z",
+    "text": "ASIC warns consumers about the risks of investing in high-yield investment programs."
+  },
+  {
+    "timestamp": "2022-02-21T09:40:15Z",
+    "text": "ASIC launches a new campaign to promote responsible lending practices."
+  },
+  {
+    "timestamp": "2022-02-21T17:05:03Z",
+    "text": "ASIC investigates a case of potential market manipulation in the derivatives market."
+  },
+  {
+    "timestamp": "2022-02-22T10:15:38Z",
+    "text": "ASIC releases a report on the performance of Australia's superannuation sector."
+  },
+  {
+    "timestamp": "2022-02-22T16:40:27Z",
+    "text": "ASIC warns consumers about the risks of investing in overseas property schemes."
+  },
+  {
+    "timestamp": "2022-02-23T11:25:12Z",
+    "text": "ASIC hosts a webinar on financial literacy for young Australians."
+  },
+  {
+    "timestamp": "2022-02-23T14:55:45Z",
+    "text": "ASIC investigates a case of potential misconduct by a credit provider."
+  },
+  {
+    "timestamp": "2022-02-24T09:40:30Z",
+    "text": "ASIC releases a guide on responsible lending obligations for mortgage brokers."
+  },
+  {
+    "timestamp": "2022-02-24T17:05:17Z",
+    "text": "ASIC publishes a report on the outcomes of its recent enforcement actions."
+  },
+  {
+    "timestamp": "2022-02-25T10:20:42Z",
+    "text": "ASIC launches a new online portal for company registration and lodgement of documents."
+  },
+  {
+    "timestamp": "2022-02-25T15:55:32Z",
+    "text": "ASIC investigates a case of potential insider trading by company executives."
+  },
+  {
+    "timestamp": "2022-02-26T09:40:27Z",
+    "text": "ASIC announces changes to the regulatory framework for credit rating agencies."
+  },
+  {
+    "timestamp": "2022-02-26T17:05:21Z",
+    "text": "ASIC's annual report on the supervision of markets and infrastructure is now available."
+  },
+  {
+    "timestamp": "2022-02-27T10:20:43Z",
+    "text": "ASIC issues a warning about a fraudulent investment scheme targeting vulnerable individuals."
+  },
+  {
+    "timestamp": "2022-02-27T15:55:32Z",
+    "text": "ASIC reminds financial advisers to update their details on the Financial Advisers Register."
+  },
+  {
+    "timestamp": "2022-02-28T09:40:29Z",
+    "text": "ASIC launches an investigation into potential breaches of continuous disclosure obligations."
+  },
+  {
+    "timestamp": "2022-02-28T17:05:17Z",
+    "text": "ASIC publishes a guide to help consumers understand the risks of margin lending."
+  },
+  {
+    "timestamp": "2022-03-01T10:20:42Z",
+    "text": "ASIC releases a consultation paper on proposed changes to crowdfunding regulations."
+  },
+  {
+    "timestamp": "2022-03-01T15:55:31Z",
+    "text": "ASIC warns consumers about the risks of investing in high-yield investment programs."
+  },
+  {
+    "timestamp": "2022-03-02T09:40:28Z",
+    "text": "ASIC launches a new campaign to promote responsible lending practices."
+  },
+  {
+    "timestamp": "2022-03-02T17:05:15Z",
+    "text": "ASIC investigates a case of potential market manipulation in the derivatives market."
+  },
+  {
+    "timestamp": "2022-03-03T11:25:11Z",
+    "text": "ASIC hosts a webinar on financial literacy for young Australians."
+  },
+  {
+    "timestamp": "2022-03-03T14:55:44Z",
+    "text": "ASIC investigates a case of potential misconduct by a credit provider."
+  },
+  {
+    "timestamp": "2022-03-04T09:40:29Z",
+    "text": "ASIC releases a guide on responsible lending obligations for mortgage brokers."
+  },
+  {
+    "timestamp": "2022-03-04T17:05:16Z",
+    "text": "ASIC publishes a report on the outcomes of its recent enforcement actions."
+  }
+]
